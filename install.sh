f436ba31791e0f9eb39f5b006c015228f833a8f0
tpalmer <tobiaspalmer93@gmail.com>
2018-03-29 14:10:30 +0200

    more tests

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "== Cloning installation"
 git clone https://github.com/nanotools/easy-nano-node.git /opt/easy-nano-node
 
 echo "== Starting installation"
-bash /opt/easy-nano-node/setup/setup.sh
+bash /opt/easy-nano-node/enn/setup.sh
