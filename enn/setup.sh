6007c0cd2e9e3cb05d7e261591393d6122fc7ebf
tpalmer <tobiaspalmer93@gmail.com>
2018-03-29 16:25:21 +0200

    serverip

--- enn/setup.sh
+++ enn/setup.sh
@@ -35,10 +35,16 @@ sed -i -e 's#\[::1\]#enn_nanonode_1#g' /opt/nanoNodeMonitor/modules/config.php
 echo "== Opening Nano Node Port"
 sudo ufw allow 7075
 
+echo ""
+
 echo -e "=== \e[31mYOUR WALLET SEED\e[39m ==="
 echo "Please write down your wallet seed to a piece of paper and store it safely!"
 docker exec enn_nanonode_1 /usr/bin/rai_node --wallet_decrypt_unsafe --wallet=$wallet
 echo -e "=== \e[31mYOUR WALLET SEED\e[39m ==="
 
+serverip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -vE '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|127\.0\.0\.1)')
+
 echo ""
-echo "All done!"
+echo "All done! *yay*"
+echo "View your Nano Node Monitor at http://$serverip"
+echo ""
