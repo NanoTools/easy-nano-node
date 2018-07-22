632cfa693e7ff522284af19e4f5d8889260a66c6
Tobias <tobiaspalmer93@gmail.com>
2018-07-22 21:03:53 +0200

    Fixed check_nanonode.sh

--- enn/check_nanonode.sh
+++ enn/check_nanonode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-response=$(curl --connect-timeout 3 --max-time 5 -g -d '{ "action": "version" }' [::1]:7076);
+curl --connect-timeout 3 --max-time 5 -g -s -d '{ "action": "version" }' [::1]:7076 > /dev/null;
 if [ $? -ne 0 ]; then
-    docker restart enn_nanonode_1
+    echo "$(date) - Restarting Nano Node..."
+    docker restart nanonode
 fi;
